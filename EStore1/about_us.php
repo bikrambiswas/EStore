290e7f10380ee1b50b7ac61607674cc2c76ffb08
BIKRAM BISWAS <88759099+bikrambiswas@users.noreply.github.com>
2021-09-12 00:51:41 +0530

    Add files via upload

---
+++ EStore1/about_us.php
@@ -0,0 +1,85 @@
+<?php
+require "includes/common.php";
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>About Us | E-Store.com</title>
+        <link rel="shortcut icon" href="img\srtcticon.png" type="image/png">
+        
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="css/style.css" rel="stylesheet">
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    
+      
+<body>
+       <!-- Header -->
+    <?php include 'includes/header.php'; ?>
+    <br>
+        <div class="container">
+            <div class="col-sm-4">
+                <div class="panel panel-default">
+                   <div class="panel-body">
+                    <h3 style="color:orange" class="color">WHO WE ARE</h3>
+                    <img src="./img/about-img.jpg" alt="about image" style="width: 325px"><br><br>
+                    <p class="text-justify">E- Store is an American Electronic E-commerce
+                                    company with headquartersin Washington.It is the largest internet based retailer in the United States.
+                                    E-Store has started as an online blog.But soon diversified,selling mobile phones. 
+                                    E-Store also sells certain low-end products like USB cables and other accessories.E-Store hasseperate retail
+                                    websites for United States,United Kinggdom and Ireland,France,Canada,Germany,Italy,Spain, Netharlands,Australia,Brazil,Japan,
+                                    India and Mexico.It also offers international shipping to other countries.<p>
+                </div>
+                </div>
+            </div>
+            
+            
+            <div class="col-sm-4">
+                <div class="panel panel-default">
+                   <div class="panel-body">
+                    <h3 style="color:orange" class="color">OUR HISTORY</h3><br><br>
+                    <h4 style="color:blue">1998&#45;</h4>
+                    <p class="text-justify">The company was founded in 1998,spurred by what Velos called his "initiaing framework"
+                                    ,which describes his efforts as an initiate participate in the internet buisness boom during that 
+                                    time.In 1998,Velos left employment as president of Ofcol & Co and moved to a seattle.
+                                    He began to work on a buisness plan for what would become  eventually E Store.</p>
+                    <br>
+                    <h4 style="color:blue">2002&#45;</h4>
+                    <p class="text-justify">In JANUARY 2012 E-Store has received a funding of $12 million
+                                from Venture Partners and Indo-US Venture Partners.</p>
+                    <br>
+                    <h4 style="color:blue">2008&#45;</h4>
+                    <p>In July 2008,the company raised a further $45 million from
+                                Bessemer Venture Partners,along with existing investors Venture Partners and Indo-US Venture
+                                Partners.</p>
+                    </div>
+                    
+                </div>
+                </div>
+            
+            
+            <div class="col-sm-4">
+                <div class="panel panel-default">
+                   <div class="panel-body">
+                    <h3 style="color:orange" class="color">OPPORTUNITIES</h3><br><br>
+                        <b>Available Roles</b>
+                            <ol>
+                                <li>Jr./Sr. Web Developer [Full Time Role + also available as a 6 Months Internships]</li>
+                                <li>Bussiness Apprentise [6 Months Internships]</li>
+                                <li>Manager at backend operations[Full Time Role + also available as a 6 Months Internships]</li>
+                            </ol>
+                        </div>
+                </div>
+            </div>
+            
+        </div>
+        <?php include 'includes/footer.php'; ?>
+    </body>
+</html>
